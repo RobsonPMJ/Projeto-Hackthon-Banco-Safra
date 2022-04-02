9c18c88c254281beabd7855a6518e03e0e006ddc
Robson Junior <robson.moura.junior@gmail.com>
2022-04-02 18:15:31 -0300

    Novas telas

--- projetobancosafra/src/components/Menu.vue
+++ projetobancosafra/src/components/Menu.vue
@@ -1,32 +1,5 @@
 <template>
-    <header class="topbar compact">
-        <div class="topbar-wrapper">
-            <div class="s-container">
-                <div class="s-row">
-                    <div class="s-col-12 flexible">
-                        <nav aria-label="Menu Superior" class="navigation-top flexible">
-                            <ul>
-                                <li>
-                                    <a class="pf" title="Para você" href="/">Para você</a>
-                                </li>
-                                 <li>
-                                    <a class="pj" title="Para sua empresa" href="/pessoa-juridica.htm">
-                                        Para sua empresa
-                                    </a>
-                                </li>
-                            </ul>   
-                        </nav>
-                        <section class="login flexible s-d-none s-d-lg-flex" id="login-header">
-                            <div class="selector flexible">
-
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </header>
+    
 </template>
 
 <script>
